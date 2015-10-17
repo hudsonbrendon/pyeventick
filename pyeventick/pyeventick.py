68a5c271fd3b4c65c3f09703bcdb284510b00664
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2015-10-17 13:59:35 -0300

    authentication fineshed

---
+++ pyeventick/pyeventick.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from requests.auth import HTTPBasicAuth
+from exception import ConectionError, AuthenticationError
+
+
+URL = 'https://www.eventick.com.br/api/v1/'
+
+class Eventick(object):
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+        try:
+            self.__token = requests.get(URL + 'tokens.json', auth=HTTPBasicAuth(self.email, self.password)).json()
+        except:
+            raise AuthenticationError('Authentication failed!')
+
+    def get_token(self):
+        '''Return token or credentials'''
+        if self.__token:
+            return (self.__token.values()[0], '')
+        else:
+            return (self.email, self.password)
+
+    
