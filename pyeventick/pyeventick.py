c6fa81a014fcc53199625050421978f1f4f218d2
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2017-02-01 17:32:59 -0300

    Adicionado pontos finais nas docstrings

--- pyeventick/pyeventick.py
+++ pyeventick/pyeventick.py
@@ -8,7 +8,9 @@ import json
 
 URL = 'https://www.eventick.com.br/api/v1/'
 
+
 class Eventick(object):
+
     def __init__(self, email, password):
         self.__email = email
         self.__password = password
@@ -19,7 +21,7 @@ class Eventick(object):
             raise AuthenticationError('Authentication failed!')
 
     def get_token(self):
-        '''Return a token or credentials'''
+        '''Return a token or credentials.'''
         if self.__token:
             return (self.__token.values()[0], '')
         else:
@@ -30,7 +32,7 @@ class Eventick(object):
         return URL + '{}'.format(url)
 
     def events(self):
-        '''Returns a json with the list of events'''
+        '''Returns a json with the list of events.'''
         try:
             request = requests.get(self.get_api_url('events.json'), auth=self.get_token()).json()
         except:
@@ -38,7 +40,7 @@ class Eventick(object):
         return request
 
     def event(self, event_id):
-        '''Returns a json with iformations of a event'''
+        '''Returns a json with iformations of a event.'''
         try:
             request = requests.get(self.get_api_url('events/{}.json').format(event_id), auth=self.get_token()).json()
         except:
@@ -57,7 +59,7 @@ class Eventick(object):
         return request
 
     def attendee(self, event_id, ID):
-        '''Returns a json with the information of a participant of the event'''
+        '''Returns a json with the information of a participant of the event.'''
         try:
             request = requests.get(self.get_api_url('events/{}/attendees/{}.json').format(event_id, ID), auth=self.get_token()).json()
         except:
@@ -65,7 +67,7 @@ class Eventick(object):
         return request
 
     def checkin(self, event_id, code, checked_at):
-        '''Performs the check in of a participant in an event'''
+        '''Performs the check in of a participant in an event.'''
         try:
             request = requests.put(self.get_api_url('events/{}/attendees/{}.json?checked_at={}').format(event_id, code, checked_at), auth=self.get_token())
         except:
